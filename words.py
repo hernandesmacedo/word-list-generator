f30b3fe0f08ec4ad36ee2120a6a6b993092ec028
hernandesmacedo <hcarneiromacedo@gmail.com>
2022-06-10 16:38:01 -0300

    Minor adjusts
    Renaming words

--- words.py
+++ words.py
@@ -1,6 +1,6 @@
 def acronym(words: list):
     """
-    Função que gera acrônimos a partir das palavras dadas como entrada
+    Gera acrônimos a partir das palavras dadas como entrada
 
     Args:
         words (list): Lista de strings contendo as palavras dadas como entrada
@@ -8,16 +8,16 @@ def acronym(words: list):
     Returns:
         str: Acrônimo das palavras dadas como entrada
     """
-    return ''.join([letter[0] for letter in words])
+    return ''.join([word[0] for word in words])
 
 def large_words_acronym(words: list):
     """
-    Função que gera acrônimos a partir das palavras dadas como entrada, considerando apenas palavras com 4 letras ou mais
+    Função que gera acrônimos a partir das palavras dadas como entrada, considerando apenas palavras com 4 caracteres ou mais
 
     Args:
         words (list): Lista de strings contendo as palavras dadas como entrada
 
     Returns:
-        str: Acrônimo das palavras com 4 letras ou mais dadas como entrada
+        str: Acrônimo das palavras com 4 caracteres ou mais dadas como entrada
     """
     return ''.join([word[0] if len(word) > 3 else '' for word in words])
