37ca850a9c85bcb8148fd53c1f8d7164ee3b33a3
hernandesmacedo <hcarneiromacedo@gmail.com>
2022-06-10 15:28:10 -0300

    Large words acronym generator created

--- words.py
+++ words.py
@@ -1,2 +1,5 @@
 def acronym(words: list):
     return ''.join([letter[0] for letter in words])
+
+def large_words_acronym(words: list):
+    return ''.join([word[0] if len(word) > 3 else '' for word in words])
